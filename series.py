f68925b203ccbae1d9cb986ffe64acaaa23cd1be
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-16 14:55:36 -0700

    fix syntax error in series.py

--- series.py
+++ series.py
@@ -16,16 +16,12 @@ def lucas(n):
         return 1
     return lucas(n - 2) + lucas(n - 1)
 
+
 def sum_series(n, a=0, b=1):
-    """make function that work with both lucas and fibonacci"
-    if n  == 0:
+    """make function that work with both lucas and fibonacci"""
+    if n == 0:
         return a
     if  n == 1:
         return b
     return sum_series(n - 2) + sum_series(n - 1)
-
-
-sum_series(n)
-sum_series(n, 2, 1)
-
 
