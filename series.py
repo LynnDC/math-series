056aa07e08d6c3a04de13c0cca54a15f2dd46438
Kurt <kurtrm@gmail.com>
2017-05-16 14:09:08 -0700

    Added the lucas function and test.

--- series.py
+++ series.py
@@ -1,9 +1,17 @@
 """ Define fibonacci and lucas function"""
 
 
-def fibonacci1(n):
+def fibonacci(n):
     """def fibonacci function using recursion"""
     if n <= 1:
         return n
-    return fibonacci1(n-2) + fibonacci1(n-1)
+    return fibonacci(n - 2) + fibonacci(n - 1)
 
+
+def lucas(n):
+    """lucas function using recursion"""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    return lucas(n - 2) + lucas(n - 1)
--- test_series.py
+++ test_series.py
@@ -1,4 +1,13 @@
-""" Tests for fibonacci"""
+"""Tests for fibonacci"""
+
+
 def test_fibonacci():
-    from series import fibonacci1
-    assert fibonacci1(7) == 13
+    """Test fibonacci"""
+    from series import fibonacci
+    assert fibonacci(7) == 13
+
+
+def test_lucas():
+    """Test lucas"""
+    from series import lucas
+    assert lucas(7) == 29
